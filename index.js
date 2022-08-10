62fe6a61a68e26ea3367d1e32a3e3a2c8b3c71a9
yamaso-burger <apple_1228_ringo@yahoo.co.jp>
2022-08-10 14:02:40 +0900

    distribute sounds to each button

--- index.js
+++ index.js
@@ -5,9 +5,41 @@ var tom1 = new Audio('sounds/tom-1.mp3');
 
 for (var i = 0; i < numberOfDrumButtons; i++) {
     document.querySelectorAll(".drum")[i].addEventListener("click", function (){
-        console.log(this);
-        this.style.color = "white";
-        console.log(this.style.color);
+        
+        var buttonInnerHTML = this.innerHTML
+        switch (buttonInnerHTML) {
+            case "w":
+                var audio = new Audio('sounds/tom-1.mp3');
+                audio.play();
+                break;
+            case "a":
+                var audio = new Audio('sounds/tom-3.mp3');
+                audio.play();
+                break;
+            case "s":
+                var audio = new Audio('sounds/tom-4.mp3');
+                audio.play();
+                break;
+            case "d":
+                var audio = new Audio('sounds/tom-2.mp3');
+                audio.play();
+                break;
+            case "j":
+                var audio = new Audio('sounds/snare.mp3');
+                audio.play();
+                break;
+            case "k":
+                var audio = new Audio('sounds/crash.mp3');
+                audio.play();
+                break;
+            case "l":
+                var audio = new Audio('sounds/kick-bass.mp3');
+                audio.play();
+                break;
+            
+            default:
+                console.log("nope");
+        }
 
         // What to do when click detected
     });
