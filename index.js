d1591ed2d108c715eef166128bfb775d9a60845a
yamaso-burger <apple_1228_ringo@yahoo.co.jp>
2022-08-10 13:39:14 +0900

    some code about this

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ var tom1 = new Audio('sounds/tom-1.mp3');
 
 for (var i = 0; i < numberOfDrumButtons; i++) {
     document.querySelectorAll(".drum")[i].addEventListener("click", function (){
-        tom1.play();
+        console.log(this);
+        this.style.color = "white";
+        console.log(this.style.color);
 
         // What to do when click detected
     });
