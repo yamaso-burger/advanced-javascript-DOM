84446b20d729ee8be3b89397c03de6d99fb70919
yamaso-burger <apple_1228_ringo@yahoo.co.jp>
2022-08-10 15:25:10 +0900

    add class method in objectsPractice.js

--- objectsPractice.js
+++ objectsPractice.js
@@ -8,11 +8,28 @@ var houseKeeper1 = {
 
 // Construnctor Function
 
-function HouseKeeper (name, age, yearsOfExperience, cleaningRepertoire) {
+function houseKeeper (name, age, yearsOfExperience, cleaningRepertoire) {
     this.name = name;
     this.age = age;
     this.yearsOfExperience = yearsOfExperience;
     this.cleaningRepertoire = cleaningRepertoire;
+    // Method
+    this.clean = function(target){
+        var canClean = false;
+        this.cleaningRepertoire.forEach((cleanSite)=>{
+            if(target === cleanSite){
+                canClean = true;
+            }
+        });
+
+        if(canClean){
+            alert("Yes sir, I will clean as soon as possible.");
+            alert(target + " is completely cleaned.");
+        } else {
+            alert("Sorry sir, I'm afraid but I can't clean " + target);
+        }
+        
+    }
 }
 
 function BellBoy (name, age, hasWorkPermit, language) {
@@ -24,6 +41,6 @@ function BellBoy (name, age, hasWorkPermit, language) {
 
 // Create Object from Construnctor
 
-var houseKeeper2 = new HouseKeeper("Aimy", 27, 5, ["bathroom", "diningroom"]);
+var houseKeeper2 = new houseKeeper("Aimy", 27, 5, ["bathroom", "diningroom"]);
 
-var bellBoy2 = new BellBoy("Tom", 23, true, ["English"]);
+var BellBoy2 = new BellBoy("Tom", 23, true, ["English"]);
